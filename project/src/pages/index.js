96ba0b4c714a5dd7c935a92e4020d01cc90523b5
Yashashvi65 <58688602+Yashashvi65@users.noreply.github.com>
2020-09-09 23:30:23 +0530

    add project

---
+++ project/src/pages/index.js
@@ -0,0 +1,41 @@
+import React from "react"
+
+import HeroSection from '../components/Reusable/HeroSection'
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import { graphql } from 'gatsby'
+import Heading from "../components/Reusable/Heading"
+import infoblock from "../components/Reusable/infoblock"
+const IndexPage = ({data}) => (
+  <Layout>
+    <SEO title="Home"/>
+  <HeroSection
+  img={data.imgg.childImageSharp.fluid}
+  title="I write Code"
+  subtitle="LearnCodeOnline.in"
+  heroclass="hero-background"
+  
+  />  
+  <infoblock heading="About Us" />
+  <infoblock heading="About Us" />
+  <infoblock heading="About Us" />
+  <infoblock heading="About Us" />
+  </Layout>
+    
+ 
+ )
+ export const query=graphql`
+ {
+  
+  imgg: file(relativePath:{ eq: "heromain.png" }) {
+       childImageSharp {
+         fluid(maxWidth:400 ,maxHeight:250) {
+          ...GatsbyImageSharpFluid_tracedSVG
+         }
+       }
+     }
+   }
+  ` 
+
+export default IndexPage

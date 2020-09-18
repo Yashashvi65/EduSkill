9fff5b291a4269f28639b5317506b0f17f1ac780
Yashashvi65 <58688602+Yashashvi65@users.noreply.github.com>
2020-09-19 00:02:30 +0530

    n

--- project/src/components/Cart/Coursecart.js
+++ project/src/components/Cart/Coursecart.js
@@ -14,13 +14,13 @@ export default class Coursecart extends Component {
          <section className="py-5 ">
              <div className="container">
                  <Heading title="Courses"/>
-                 <div className="row ">
+                 <div className="row py-5">
                    {this.state.mycourses.map(({node})=>{
                  return(
                     <div key={node.id}
-                    className="col-11 col-md-6 d-flex mx-auto">
+                    className="col-11 col-md-6 d-flex mx-auto py-2">
                         <Img fixed={node.image.fixed}/>
-                        <div className="flex-grow-1 px-3">
+                        <div className="flex-grow-1 px-3 py-12">
                             <div className="d-flex ">
                             
                             <h6 className="mb-0">
@@ -28,7 +28,7 @@ export default class Coursecart extends Component {
 
                             </h6>
                             <h6 className="mb-0 text-success ml-3">
-                                ${node.price}
+                                $${node.price}
 
                             </h6>
                             </div>
